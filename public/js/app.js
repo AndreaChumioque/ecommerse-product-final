22e585b96fce07e12bd7d89033028915ce0e0980
andrea-isabel <andrea19.93.15@gmail.com>
2018-03-02 18:11:39 -0500

    peticiones basicas

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
   // Funciones
+
+  // Obtener subcategorias
+  $.get('https://api.mercadolibre.com/categories/MPE1071', function(response) {
+    for (const subcategory of response.children_categories) {
+      const li = `<li class="nav-item">
+                    <a class="nav-link" href="#">${subcategory.name}</a>
+                  </li>`;
+      $('#navbarNav .navbar-nav').append(li);
+    }
+  });
+
+
+  $.ajax({
+    url: 'https://api.mercadolibre.com/sites/MPE/search?category=MPE1071', // cambiar el id
+    success: function(data){
+      // console.log(data.results)
+    }
+  });
 });
