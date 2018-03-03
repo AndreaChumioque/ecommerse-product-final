1ae974cfd3cf02eae66963fa9d5e63ff20557082
andrea-isabel <andrea19.93.15@gmail.com>
2018-03-02 19:15:46 -0500

    productos

--- public/js/app.js
+++ public/js/app.js
@@ -11,10 +11,26 @@ $(document).ready(function() {
     }
   });
 
-
+  // Obetener todos los productos
   $.ajax({
-    url: 'https://api.mercadolibre.com/sites/MPE/search?category=MPE1071', // cambiar el id
+    url: 'https://api.mercadolibre.com/sites/MPE/search?category=MPE1071',
     success: function(data){
+      for (const item of data.results) {
+        const div = `<div class="col-12 col-sm-6 col-md-4 d-flex mb-3">
+
+                        <div class="card col-12">
+                          <img class="card-img-top" src="${item.thumbnail}" alt="Card image cap">
+                          <div class="card-body">
+                            <h6 class="card-title font-weight-bold">${item.title}</h6>
+                            <h6 class="card-text">${item.price} ${item.installments.currency_id}</h6>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                          </div>
+                        </div>
+
+                    </div>`;
+
+        $('#general-products').append(div);
+      }
       // console.log(data.results)
     }
   });
